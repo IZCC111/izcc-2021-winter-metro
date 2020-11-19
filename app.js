98bfae27dd2e5341dd4f979fca99fe4445fedd96
TGoC <61151352+charlie-ma-717@users.noreply.github.com>
2020-11-20 00:40:34 +0800

    update

---
+++ app.js
@@ -0,0 +1,72 @@
+const { google } = require('googleapis');
+const keys = require('./keys.json');
+const ejs = require('ejs');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+
+
+const client = new google.auth.JWT(
+    keys.client_email,
+    null,
+    keys.private_key,
+    ['https://www.googleapis.com/auth/spreadsheets.readonly']
+);
+
+
+client.authorize(function (err, tokens) {
+
+    if (err) {
+        console.log(err);
+        return;
+    } else {
+        console.log('Connected!');
+        gsrun(client);
+    }
+});
+
+
+async function gsrun(cl) {
+    const gsapi = google.sheets({ version: 'v4', auth: cl });
+
+
+    const optlati = {
+        spreadsheetId: '18f7rUUJ_0Vq7IJ20v4Rm_uPp75g0aXHNsjWLqNnW6Ec',
+        range: 'coordinates!B1:B90'
+    };
+    const optlong = {
+        spreadsheetId: '18f7rUUJ_0Vq7IJ20v4Rm_uPp75g0aXHNsjWLqNnW6Ec',
+        range: 'coordinates!C1:C90'
+    };
+    const opteki = {
+        spreadsheetId: '18f7rUUJ_0Vq7IJ20v4Rm_uPp75g0aXHNsjWLqNnW6Ec',
+        range: 'coordinates!A1:A90'
+    };
+
+
+    app.get('/', async function (req, res) {
+        let lati = await gsapi.spreadsheets.values.get(optlati);
+        let long = await gsapi.spreadsheets.values.get(optlong);
+        let eki = await gsapi.spreadsheets.values.get(opteki);
+        let latiArray = [];
+        let longArray = [];
+        let ekiArray = [];
+        for(let i = 0 ; i < lati.data.values.length ; i++){
+            latiArray[i] = lati.data.values[i][0];
+            longArray[i] = long.data.values[i][0];
+            ekiArray[i] = eki.data.values[i][0];
+        }
+        console.log(ekiArray);
+        res.render('index', { latitude: latiArray ,longitude: longArray ,len: latiArray.length ,eki: ekiArray});
+    });
+
+};
+
+
+app.listen(7122, () => console.log('Server up and running'));
