308d626d1f4af924c8e5305054ac3f20d016a6f1
TGoC <61151352+charlie-ma-717@users.noreply.github.com>
2020-11-20 18:48:21 +0800

    update limit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { google } = require('googleapis');
+const {google} = require('googleapis');
 const keys = require('./keys.json');
 const ejs = require('ejs');
 const express = require('express');
@@ -8,8 +8,6 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-
 
 
 const client = new google.auth.JWT(
@@ -33,7 +31,7 @@ client.authorize(function (err, tokens) {
 
 
 async function gsrun(cl) {
-    const gsapi = google.sheets({ version: 'v4', auth: cl });
+    const gsapi = google.sheets({version: 'v4', auth: cl});
 
 
     const optlati = {
@@ -48,25 +46,42 @@ async function gsrun(cl) {
         spreadsheetId: '18f7rUUJ_0Vq7IJ20v4Rm_uPp75g0aXHNsjWLqNnW6Ec',
         range: 'coordinates!A1:A90'
     };
+    const optlimit = {
+        spreadsheetId: '18f7rUUJ_0Vq7IJ20v4Rm_uPp75g0aXHNsjWLqNnW6Ec',
+        range: 'coordinates!G2:G5'
+    };
 
 
     app.get('/', async function (req, res) {
         let lati = await gsapi.spreadsheets.values.get(optlati);
         let long = await gsapi.spreadsheets.values.get(optlong);
         let eki = await gsapi.spreadsheets.values.get(opteki);
+        let limit = await gsapi.spreadsheets.values.get(optlimit);
         let latiArray = [];
         let longArray = [];
         let ekiArray = [];
-        for(let i = 0 ; i < lati.data.values.length ; i++){
+        for (let i = 0; i < lati.data.values.length; i++) {
             latiArray[i] = lati.data.values[i][0];
             longArray[i] = long.data.values[i][0];
             ekiArray[i] = eki.data.values[i][0];
         }
-        console.log(ekiArray);
-        res.render('index', { latitude: latiArray ,longitude: longArray ,len: latiArray.length ,eki: ekiArray});
+        let north = limit.data.values[0][0];
+        let east = limit.data.values[1][0];
+        let south = limit.data.values[2][0];
+        let west = limit.data.values[3][0];
+        res.render('index', {
+            latitude: latiArray,
+            longitude: longArray,
+            len: latiArray.length,
+            eki: ekiArray,
+            north: north,
+            east: east,
+            south: south,
+            west: west
+        });
     });
 
 };
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Server up and running'));
+app.listen(PORT, () => console.log('Server up and running on ' + process.env.PORT + ' or 3000'));
