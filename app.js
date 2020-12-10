9ff76af20c4ad8d27166fa2c4ec2973c3efa8f65
TGoC_virus <61151352+TGoC-virus@users.noreply.github.com>
2020-12-10 13:46:43 +0800

    half finish cut

--- app.js
+++ app.js
@@ -38,50 +38,62 @@ async function gsrun(cl) {
 
     const optlati = {
         spreadsheetId: '18f7rUUJ_0Vq7IJ20v4Rm_uPp75g0aXHNsjWLqNnW6Ec',
-        range: 'coordinates!B1:B90'
+        range: 'coordinates!B1:B98'
     };
     const optlong = {
         spreadsheetId: '18f7rUUJ_0Vq7IJ20v4Rm_uPp75g0aXHNsjWLqNnW6Ec',
-        range: 'coordinates!C1:C90'
+        range: 'coordinates!C1:C98'
     };
     const opteki = {
         spreadsheetId: '18f7rUUJ_0Vq7IJ20v4Rm_uPp75g0aXHNsjWLqNnW6Ec',
-        range: 'coordinates!A1:A90'
+        range: 'coordinates!A1:A98'
     };
-    const optlimit = {
+    const optbgc = {
         spreadsheetId: '18f7rUUJ_0Vq7IJ20v4Rm_uPp75g0aXHNsjWLqNnW6Ec',
-        range: 'coordinates!G2:G5'
+        range: 'coordinates!D1:D98'
     };
-
+    const opttopic = {
+        spreadsheetId: '18f7rUUJ_0Vq7IJ20v4Rm_uPp75g0aXHNsjWLqNnW6Ec',
+        range: 'topic!B1:B98'
+    };
 
     app.get('/', async function (req, res) {
         let lati = await gsapi.spreadsheets.values.get(optlati);
         let long = await gsapi.spreadsheets.values.get(optlong);
         let eki = await gsapi.spreadsheets.values.get(opteki);
-        let limit = await gsapi.spreadsheets.values.get(optlimit);
+        let bgc = await gsapi.spreadsheets.values.get(optbgc);
+
         let latiArray = [];
         let longArray = [];
         let ekiArray = [];
+        let bgcArray = [];
+        let topicArray = [];
         for (let i = 0; i < lati.data.values.length; i++) {
             latiArray[i] = lati.data.values[i][0];
             longArray[i] = long.data.values[i][0];
             ekiArray[i] = eki.data.values[i][0];
+            bgcArray[i] = bgc.data.values[i][0];
+            topicArray[i] = topic.data.values[i][0];
         }
-        let north = limit.data.values[0][0];
-        let east = limit.data.values[1][0];
-        let south = limit.data.values[2][0];
-        let west = limit.data.values[3][0];
         res.render('index', {
             latitude: latiArray,
             longitude: longArray,
             len: latiArray.length,
             eki: ekiArray,
-            north: north,
-            east: east,
-            south: south,
-            west: west
+            bgc: bgcArray,
         });
     });
+    app.get('/map',async (req, res) => {
+        let topic = await gsapi.spreadsheets.values.get(opttopic);
+        let eki = await gsapi.spreadsheets.values.get(opteki);
+        let map={ekiArray:[],topicArray:[]}
+        for (let i = 0; i < eki.data.values.length; i++) {
+            map.ekiArray[i] = eki.data.values[i][0];
+            map.topicArray[i] = topic.data.values[i][0];
+        }
+        res.send(map)
+
+    })
 
 };
 
